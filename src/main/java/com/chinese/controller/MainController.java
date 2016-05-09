5252fbf18369bf7c52bce3fc577d4f841ea28bf9
Ranunculus <tatinamalyutina@gmail.com>
2016-05-09 22:24:27 +0300

    Trying to make the project work again

--- src/main/java/com/chinese/controller/MainController.java
+++ src/main/java/com/chinese/controller/MainController.java
@@ -1,7 +1,14 @@
 package com.chinese.controller;
 
+import de.neuland.jade4j.JadeConfiguration;
+import de.neuland.jade4j.template.JadeTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by Ranunculus on 29.04.2016.
@@ -9,7 +16,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-    @RequestMapping("/main")
-    public void showMainModel(){
+    private JadeConfiguration jadeConfiguration;
+
+    @RequestMapping("/")
+    public String showMainModel(){
+        try {
+            ModelAndView modelAndView = new ModelAndView("main");
+            JadeTemplate template = jadeConfiguration.getTemplate(modelAndView.getViewName());
+            System.out.println(jadeConfiguration.renderTemplate(template, modelAndView.getModel()));
+            return jadeConfiguration.renderTemplate(template, modelAndView.getModel());
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+
+    }
+
+    public JadeConfiguration getJadeConfiguration() {
+        return jadeConfiguration;
+    }
+
+    public void setJadeConfiguration(JadeConfiguration jadeConfiguration) {
+        this.jadeConfiguration = jadeConfiguration;
     }
 }
