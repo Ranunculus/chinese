0dfcf178d276b85234b06a489b414104d060cd0f
Ranunculus <tatianamalyutina@gmail.com>
2016-06-09 12:18:20 +0300

    Fixed encoding bug

--- src/main/java/com/chinese/controller/MainController.java
+++ src/main/java/com/chinese/controller/MainController.java
@@ -4,12 +4,10 @@ import de.neuland.jade4j.JadeConfiguration;
 import de.neuland.jade4j.template.JadeTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * Created by Ranunculus on 29.04.2016.
@@ -19,12 +17,12 @@ public class MainController {
 
     private JadeConfiguration jadeConfiguration;
 
-    @RequestMapping("/")
+    @RequestMapping(value="/", produces = "text/html;charset=UTF-8")
     @ResponseBody
     public String showMainModel(){
         try {
             ModelAndView e = new ModelAndView("layout/layout");
-            e.getModel().put("title", "Finally");
+            e.getModel().put("myText", "你好，近看! 这是编程人员的博客。我决定委弃我的在公司的工作，因为我要看别的外国和人");
             JadeTemplate template = jadeConfiguration.getTemplate(e.getViewName());
             return jadeConfiguration.renderTemplate(template, e.getModel());
         } catch (IOException e) {
